33df933c220f578e9da059122b639b28aba2f483
inf7m <dpnt1510@gmail.com>
2023-05-05 11:21:43 +0700

    Append __ini__.py in todo package

---
+++ todo/__init__.py
@@ -0,0 +1,12 @@
+# Top-level package
+
+app_name = 'SETUP-workspace'
+version = "1.0"
+
+ERRORS = dict(
+    dir_err= "directory-error",
+    FILE_ERROR= "File-error",
+    DB_READ_ERROR= "DBRn-error",
+    DB_WRITE_ERROR= "DBWn-error",
+    ID_ERROR= "ID-error",
+)
